6daf0c1990722efd407a7243551518cfb38b665c
schamberlain <myrmecocystus@gmail.com>
2014-01-03 09:15:33 -0800

    added crsearch functionality

--- doiref.gemspec
+++ doiref.gemspec
@@ -1,7 +1,7 @@
 Gem::Specification.new do |s|
   s.name        = 'doiref'
   s.version     = '0.0.1'
-  s.date        = '2013-06-20'
+  s.date        = '2014-01-03'
   s.summary     = "Gets citations from DOIs"
   s.description = "Gets citations from DOIs, like bibtex entries"
   s.authors     = ["Scott Chamberlain"]
@@ -9,7 +9,7 @@ Gem::Specification.new do |s|
   s.files       = ["lib/doiref.rb"]
   s.homepage    = 'http://github.com/schamberlain/doiref'
   s.licenses    = 'CC0'
-  s.executables << 'doiref'
+  s.executables = ['doiref', 'crsearch']
   s.add_dependency('bibtex-ruby', '>= 2.3.2')
   s.add_dependency('httparty', '>= 0.11.0')
 end
--- lib/doiref.rb
+++ lib/doiref.rb
@@ -1,5 +1,6 @@
 require 'bibtex'
 require 'httparty'
+require 'json'
 
 class DOIref	
 	def self.lookup_bib(doi, style='bibtex')
@@ -10,3 +11,36 @@ class DOIref
 	end
 end
 # lookup_bib(input)
+
+class CRSearch
+	def self.cr_search(query)
+		query = [query]
+		url = "http://search.labs.crossref.org/links"
+		out = 
+			HTTParty.post(url, 
+				:body => query.to_json, 
+				:headers => { "Content-Type" => "application/json"}
+			)
+		if out.code == 200
+			nil
+		else
+			puts "ERROR #{out.code}"
+		end
+		tt = out['results']
+		coll = []
+		tt.each do |item|
+			gg = item['doi']
+			if gg!=nil
+				gg = gg.sub('http://dx.doi.org/', '')
+			end
+			coll << 
+			{
+				'match'=>item['match'], 
+				'doi'=>gg,
+				'text'=>item['text']
+			}
+		end
+        coll.display
+        # coll.each { |i| i.display }
+	end
+end
